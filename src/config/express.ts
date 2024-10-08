c5a63d0da336a6138c6b1fd157aef26a3812e403
abneribeiro <eliasabner38@gmail.com>
2024-10-08 11:35:59 +0100

    Add: express configuration file

---
+++ src/config/express.ts
@@ -0,0 +1,12 @@
+// config/express.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+module.exports = app;
